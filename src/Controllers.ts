89b98e49bff921fa398d161e20fc97191bdeb06f
fmolliet <fmolliet@hotmail.com>
2021-11-21 10:39:31 -0300

    Adicionado validação para corpo da requisição

--- src/Controllers.ts
+++ src/Controllers.ts
@@ -1,24 +1,25 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import RequestDTO from './dto/Request';
+import HttpException from './helpers/HttpException';
 
 import _service from './Services';
 
 export default class Controller {
     
     
-    async create(req : RequestDTO, res: Response): Promise<Response>{
+    async create(req : RequestDTO, res: Response, next: NextFunction): Promise<Response|void>{
         
-        try{
-            const result = {}// await _service.verify(req.body);
+        const result = {}// await _service.verify(req.body);
             
-            const urls = req.body.urls;
+        if (!req.body.urls || req.body.urls.length === 0)
+            return next(new HttpException(400,'Nenhuma url foi enviada para processamento!'));
+        
+        const urls = req.body.urls;    
             
-            console.log(urls)
+        console.log(urls)
             
-            return res.status(200).json(result)
-        }catch (err: any){
-            return res.status(500).json({message: err.message ? err.message : err});
-        }
+        return res.status(200).json(result)
+        
     }
 
 }
